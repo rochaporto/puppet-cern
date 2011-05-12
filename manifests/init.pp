e4523f4f68641e9c1c1bbb4ba6950f1cf5ca727e
root <root@vmdm0006.cern.ch>
2011-05-12 16:33:43 +0200

    Added import of manifest files in init.pp.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,5 @@
+import '*.pp*'
+
 class cern {
   # TODO: Automatically load the required class defining the yum repos (slc5, ...)
   # We should do this based on some auto recognition of the machine's system  
